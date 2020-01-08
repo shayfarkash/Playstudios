3f10f302d245ce12b8001368909f31bed309760d
shayfarkash <32242281+shayfarkash@users.noreply.github.com>
2020-01-08 10:39:40 +0200

    Update backupandsync.rb

--- backupandsync.rb
+++ backupandsync.rb
@@ -1,9 +1,9 @@
-cask 'BackupAndSync' do
+cask 'backupandsync' do
   version :latest
   sha256 :no_check
 
   url "http://api01/InstallBackupAndSync.dmg.zip"
-  name 'BackupAndSync'
+  name 'backupandsync'
   
 
   app ''
